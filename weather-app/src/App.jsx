119d0737c91d20f5fa32d3cacf4b1f50ce119372
Marius Ranta <mariusranta@gmail.com>
2025-03-27 17:49:51 +0200

    New components

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import CityList from './CityList'
 
 const App = () => {
   return (
-    <div className="text-center bg-black">
-      <h1 className="text-8xl text-blue-200">
-        Hello, Tailwind CSS!
+    <div className="text-center bg-blue">
+      <h1 className="text-8xl text-white">
+        Amazing Weather App!
       </h1>
-      <p className="text-2xl mt-10 text-red-500">Hello</p>
+      <p className="text-2xl mt-10 text-red-500">Under Maintenance ;D</p>
     </div>
   );
 }
